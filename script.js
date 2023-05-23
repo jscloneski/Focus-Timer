86f85900df53cdce2e1432756a3a7d5102321752
João Scloneski <jscloneski.dev@gmail.com>
2023-05-23 20:05:36 -0300

    Lógica nos botões

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-let play = document.querySelector('.play')
-let pause = document.querySelector('.pause')
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
 
-play.addEventListener('click', function() {
-    play.classList.add('hide')
-    pause.classList.remove('hide')
+buttonPlay.addEventListener('click', function() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
 })
 
-pause.addEventListener('click', function() {
-    pause.classList.add('hide')
-    play.classList.remove('hide')
+buttonPause.addEventListener('click', function() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', function(){
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+})
+
+buttonSoundOff.addEventListener('click', function() {
+    buttonSoundOn.classList.remove('hide')
+    buttonSoundOff.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click', function() {
+    buttonSoundOff.classList.remove('hide')
+    buttonSoundOn.classList.add('hide')
 })
