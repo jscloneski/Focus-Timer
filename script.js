92443aa61aadc0329385c01b9c87e80d4a85fa09
João Scloneski <jscloneski.dev@gmail.com>
2023-05-25 21:01:50 -0300

    Zerando o cronômetro

--- script.js
+++ script.js
@@ -11,13 +11,30 @@ const secondsDisplay = document.querySelector('.seconds')
 function countdown(){
     setTimeout(function() {
         let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
 
-        if( seconds <= 0 ) {
-            seconds = 60
+        secondsDisplay.textContent = "00"
+
+        if (minutes <= 0) {
+
+            buttonPlay.classList.remove('hide')
+            buttonPause.classList.add('hide')
+            buttonSet.classList.remove('hide')
+            buttonStop.classList.add('hide')
+
+            return
         }
 
+        if( seconds <= 0 ) {
+            seconds = 2
+
+            minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+        }
+
+        secondsDisplay.textContent = String(seconds - 1).padStart(2,"0")
+
+
         countdown()
-        secondsDisplay.textContent = seconds - 1
     }, 1000) 
 }
 
@@ -54,5 +71,5 @@ buttonSoundOn.addEventListener('click', function() {
 
 buttonSet.addEventListener('click', function() {
     minutes = prompt('Quantos minutos?')
-    minutesDisplay.textContent = minutes
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
 })
