25d1cde9282f51ce8304211095e9da625e6dd041
João Scloneski <jscloneski.dev@gmail.com>
2023-06-21 21:01:43 -0300

    clean code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import resetControls from "./controls.js"
-import { countdown, resetTimer } from "./timer.js"
+import { Timer } from "./timer.js"
 
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
@@ -13,13 +13,20 @@ const secondsDisplay = document.querySelector('.seconds')
 let minutes = Number(minutesDisplay.textContent)
 let timerTimeOut
 
+const timer = Timer({
+    minutesDisplay,
+    secondsDisplay,
+    timerTimeOut,
+    resetControls,
+})
+
 buttonPlay.addEventListener('click', function() {
     buttonPlay.classList.add('hide')
     buttonPause.classList.remove('hide')
     buttonSet.classList.add('hide')
     buttonStop.classList.remove('hide')
 
-    countdown()
+    timer.countdown()
 })
 
 buttonPause.addEventListener('click', function() {
@@ -30,7 +37,7 @@ buttonPause.addEventListener('click', function() {
 
 buttonStop.addEventListener('click', function(){
     resetControls()
-    resetTimer()
+    timer.resetTimer()
 })
 
 buttonSoundOff.addEventListener('click', function() {
@@ -46,7 +53,7 @@ buttonSoundOn.addEventListener('click', function() {
 buttonSet.addEventListener('click', function() {
     let newMinutes = prompt('Quantos minutos?')
     if (!newMinutes) {
-        resetTimer()
+        timer.resetTimer()
         return
     }
 
--- js/timer.js
+++ js/timer.js
@@ -1,37 +1,49 @@
-function updateTimerDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
-    secondsDisplay.textContent = String(seconds).padStart(2, "0")
-}
+export function Timer({
+    minutesDisplay,
+    secondsDisplay,
+    timerTimeOut,
+    resetControls
+}) {
 
-function resetTimer() {
-    updateTimerDisplay(minutes, 0)
-    clearTimeout(timerTimeOut)
-}
+    function updateTimerDisplay(minutes, seconds) {
+        minutesDisplay.textContent = String(minutes).padStart(2, "0")
+        secondsDisplay.textContent = String(seconds).padStart(2, "0")
+    }
 
-function countdown(){
-    timerTimeOut = setTimeout(function() {
-        let seconds = Number(secondsDisplay.textContent)
-        let minutes = Number(minutesDisplay.textContent)
-
+    function resetTimer() {
         updateTimerDisplay(minutes, 0)
+        clearTimeout(timerTimeOut)
+    }
 
-        if (minutes <= 0) {
+    function countdown(){
+        timerTimeOut = setTimeout(function() {
+            let seconds = Number(secondsDisplay.textContent)
+            let minutes = Number(minutesDisplay.textContent)
 
-            resetControls()
+            updateTimerDisplay(minutes, 0)
 
-            return
-        }
+            if (minutes <= 0) {
 
-        if( seconds <= 0 ) {
-            seconds = 60
-            --minutes
-        }
+                resetControls()
 
-        updateTimerDisplay(minutes, String(seconds - 1))
+                return
+            }
+
+            if( seconds <= 0 ) {
+                seconds = 60
+                --minutes
+            }
+
+            updateTimerDisplay(minutes, String(seconds - 1))
 
 
-        countdown()
-    }, 1000) 
+            countdown()
+        }, 1000) 
+    }
+
+    return {
+        countdown,
+        resetTimer,
+    }
+
 }
-
-export {  }
